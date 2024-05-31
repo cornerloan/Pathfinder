c1d3378b5ea2d0b2e56293a6b7493fc6d4cab5a3
cornerloan <connorlowe21@gmail.com>
2024-05-31 09:19:51 -0700

    final

--- src/Scenes/Pathfinder.js
+++ src/Scenes/Pathfinder.js
@@ -2,66 +2,58 @@ class Pathfinder extends Phaser.Scene {
     constructor() {
         super("pathfinderScene");
     }
-
     preload() {
     }
-
     init() {
         this.TILESIZE = 16;
         this.SCALE = 2.0;
         this.TILEWIDTH = 40;
         this.TILEHEIGHT = 25;
     }
-
     create() {
-        // Create a new tilemap which uses 16x16 tiles, and is 40 tiles wide and 25 tiles tall
-        this.map = this.add.tilemap("three-farmhouses", this.TILESIZE, this.TILESIZE, this.TILEHEIGHT, this.TILEWIDTH);
-
+// Create a new tilemap which uses 16x16 tiles, and is 40 tiles wide and 25 tiles tall
+        this.map = this.add.tilemap("three-farmhouses", this.TILESIZE,
+            this.TILESIZE, this.TILEHEIGHT, this.TILEWIDTH);
         // Add a tileset to the map
-        this.tileset = this.map.addTilesetImage("kenney-tiny-town", "tilemap_tiles");
-
+        this.tileset = this.map.addTilesetImage("kenney-tiny-town",
+            "tilemap_tiles");
         // Create the layers
-        this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
-        this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0, 0);
-        this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);
-
+        this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset,
+            0, 0);
+        this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0,
+            0);
+        this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0,
+            0);
         // Create townsfolk sprite
         // Use setOrigin() to ensure the tile space computations work well
-        my.sprite.purpleTownie = this.add.sprite(this.tileXtoWorld(5), this.tileYtoWorld(5), "purple").setOrigin(0,0);
-        
+        my.sprite.purpleTownie = this.add.sprite(this.tileXtoWorld(5),
+            this.tileYtoWorld(5), "purple").setOrigin(0, 0);
         // Camera settings
-        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels,
+            this.map.heightInPixels);
         this.cameras.main.setZoom(this.SCALE);
-
         // Create grid of visible tiles for use with path planning
-        let tinyTownGrid = this.layersToGrid([this.groundLayer, this.treesLayer, this.housesLayer]);
-
-        let walkables = [1, 2, 3, 30, 40, 41, 42, 43, 44, 95, 13, 14, 15, 25, 26, 27, 37, 38, 39, 70, 84];
-
+        let tinyTownGrid = this.layersToGrid([this.groundLayer, this.treesLayer,
+        this.housesLayer]);
+        let walkables = [1, 2, 3, 30, 40, 41, 42, 43, 44, 95, 13, 14, 15, 25, 26,
+            27, 37, 38, 39, 70, 84];
         // Initialize EasyStar pathfinder
         this.finder = new EasyStar.js();
-
         // Pass grid information to EasyStar
         // EasyStar doesn't natively understand what is currently on-screen,
         // so, you need to provide it that information
         this.finder.setGrid(tinyTownGrid);
-
         // Tell EasyStar which tiles can be walked on
         this.finder.setAcceptableTiles(walkables);
-
         this.activeCharacter = my.sprite.purpleTownie;
-
         // Handle mouse clicks
         // Handles the clicks on the map to make the character move
         // The this parameter passes the current "this" context to the
         // function this.handleClick()
-        this.input.on('pointerup',this.handleClick, this);
-
+        this.input.on('pointerup', this.handleClick, this);
         this.cKey = this.input.keyboard.addKey('C');
         this.lowCost = false;
-
     }
-
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.cKey)) {
             if (!this.lowCost) {
@@ -75,7 +67,6 @@ class Pathfinder extends Phaser.Scene {
             }
         }
     }
-
     resetCost(tileset) {
         for (let tileID = tileset.firstgid; tileID < tileset.total; tileID++) {
             let props = tileset.getTileProperties(tileID);
@@ -86,15 +77,12 @@ class Pathfinder extends Phaser.Scene {
             }
         }
     }
-
     tileXtoWorld(tileX) {
         return tileX * this.TILESIZE;
     }
-
     tileYtoWorld(tileY) {
         return tileY * this.TILESIZE;
     }
-
     // layersToGrid
     //
     // Uses the tile layer information in this.map and outputs
@@ -104,23 +92,33 @@ class Pathfinder extends Phaser.Scene {
         let grid = [];
         // Initialize grid as two-dimensional array
         // TODO: write initialization code
-
+        for (let i = 0; i < this.map.height; i++) {
+            grid[i] = [];
+        }
         // Loop over layers to find tile IDs, store in grid
         // TODO: write this loop
-
+        for (let a = 0; a < 3; a++) {
+            let arrayOfLayers = this.map.layers;
+            for (let x = 0; x < this.map.width; x++) {
+                for (let y = 0; y < this.map.height; y++) {
+                    let tile1 = arrayOfLayers[a].tilemapLayer.getTileAt(x, y);
+                    if (tile1 != null) {
+                        grid[y][x] = tile1.index;
+                    }
+                }
+            }
+        }
         return grid;
     }
-
-
     handleClick(pointer) {
         let x = pointer.x / this.SCALE;
         let y = pointer.y / this.SCALE;
-        let toX = Math.floor(x/this.TILESIZE);
-        var toY = Math.floor(y/this.TILESIZE);
-        var fromX = Math.floor(this.activeCharacter.x/this.TILESIZE);
-        var fromY = Math.floor(this.activeCharacter.y/this.TILESIZE);
-        console.log('going from ('+fromX+','+fromY+') to ('+toX+','+toY+')');
-    
+        let toX = Math.floor(x / this.TILESIZE);
+        var toY = Math.floor(y / this.TILESIZE);
+        var fromX = Math.floor(this.activeCharacter.x / this.TILESIZE);
+        var fromY = Math.floor(this.activeCharacter.y / this.TILESIZE);
+        console.log('going from (' + fromX + ',' + fromY + ') to (' + toX + ',' +
+            toY + ')');
         this.finder.findPath(fromX, fromY, toX, toY, (path) => {
             if (path === null) {
                 console.warn("Path was not found.");
@@ -133,35 +131,33 @@ class Pathfinder extends Phaser.Scene {
         // When the path computing is done, the arrow function given with
         // this.finder.findPath() will be called.
     }
-    
     moveCharacter(path, character) {
-        // Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
+// Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
         var tweens = [];
-        for(var i = 0; i < path.length-1; i++){
-            var ex = path[i+1].x;
-            var ey = path[i+1].y;
+        for (var i = 0; i < path.length - 1; i++) {
+            var ex = path[i + 1].x;
+            var ey = path[i + 1].y;
             tweens.push({
-                x: ex*this.map.tileWidth,
-                y: ey*this.map.tileHeight,
+                x: ex * this.map.tileWidth,
+                y: ey * this.map.tileHeight,
                 duration: 200
             });
         }
-    
         this.tweens.chain({
             targets: character,
             tweens: tweens
         });
-
     }
-
     // A function which takes as input a tileset and then iterates through all
-    // of the tiles in the tileset to retrieve the cost property, and then 
+    // of the tiles in the tileset to retrieve the cost property, and then
     // uses the value of the cost property to inform EasyStar, using EasyStar's
     // setTileCost(tileID, tileCost) function.
     setCost(tileset) {
         // TODO: write this function
+        for (let i = tileset.firstgid; i < tileset.total; i++) {
+            let props = tileset.getTileProperties(i);
+            let cost = props.cost;
+            this.finder.setTileCost(i, cost);
+        }
     }
-
-
-
 }
